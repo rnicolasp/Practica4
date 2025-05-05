aa748c71703b9b25e2d5760b340073067e102cd4
rnicolasp <rnicolasp@esliceu.net>
2025-05-05 20:00:55 +0200

    first commit

---
+++ LightBot.java
@@ -0,0 +1,12 @@
+ class LightBot {
+String nombre;
+LightBot(String nombre){
+    this.nombre = nombre;
+}
+
+}
+class main{
+    public static void main(String args[]){
+        LightBot pepe = new LightBot("pepe");
+    }
+}
